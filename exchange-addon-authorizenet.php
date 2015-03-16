d38292fb529982098b73025b3e132b579f8b95e2
Lew Ayotte <lew@ithemes.com>
2015-03-16 10:56:32 -0500

    Tagging 1.0.6

--- exchange-addon-authorizenet.php
+++ exchange-addon-authorizenet.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Authorize.Net Add-on
- * Version: 1.0.5
+ * Version: 1.0.6
  * Description: Adds the ability for users to checkout with Authorize.Net.
  * Plugin URI: http://ithemes.com/exchange/authorize-net/
  * Author: iThemes
