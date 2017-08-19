84ff615e823a90db96d8ec4c4b11559bca546fdd
AJ Morris <ajmorris2002@gmail.com>
2017-08-18 22:13:01 -0400

    Version bump

--- exchange-addon-authorizenet.php
+++ exchange-addon-authorizenet.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: ExchangeWP - Authorize.Net Add-on
- * Version: 1.4.4
+ * Version: 1.4.5
  * Description: Adds the ability for users to checkout with Authorize.Net.
  * Plugin URI: https://exchangewp.com/downloads/authorize-net/
  * Author: ExchangeWP
@@ -98,7 +98,7 @@ function exchange_authorizenet_plugin_updater() {
 
 	// setup the updater
 	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
-			'version' 		=> '1.4.4', 				// current version number
+			'version' 		=> '1.4.5', 				// current version number
 			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
 			'item_name' 	=> 'authorize-net', 	  // name of this plugin
 			'author' 	  	=> 'ExchangeWP',    // author of this plugin
